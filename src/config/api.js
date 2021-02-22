2a709cc11771cc7e8ddf8f208404f36f9a63d578
VSAlmeida <vinicius.sebadelhe@ibm.com>
2021-02-22 09:55:39 -0300

    Fix 3rh party api rate limit

--- src/config/api.js
+++ src/config/api.js
@@ -1,18 +1,25 @@
 const axios = require("axios");
+const rateLimit = require("axios-rate-limit");
 
-const iam = axios.default.create({
-  baseURL: "https://iam.cloud.ibm.com",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    Accept: "application/json",
-  },
-});
+const iam = rateLimit(
+  axios.default.create({
+    baseURL: "https://iam.cloud.ibm.com",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+  }),
+  { maxRequests: 10, perMilliseconds: 1000, maxRPS: 10 }
+);
 
-const userManagement = axios.default.create({
-  baseURL: "https://user-management.cloud.ibm.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+const userManagement = rateLimit(
+  axios.default.create({
+    baseURL: "https://user-management.cloud.ibm.com",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }),
+  { maxRequests: 10, perMilliseconds: 1000, maxRPS: 10 }
+);
 
 module.exports = { iam, userManagement };
