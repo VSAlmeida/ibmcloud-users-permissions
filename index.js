75428061353d4d27142a32e6560e808fea86bbc4
VSAlmeida <vinicius.sebadelhe@ibm.com>
2021-02-22 02:36:35 -0300

    Added Progress Bar

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 require("dotenv").config();
-const { getToken, getUsers, getAccessGroups } = require("./src");
+const { getToken, getUsers, getAccessGroups, ProgressBar } = require("./src");
+const progress = new ProgressBar("Getting Permissions");
 
 const accountId = process.env.ACCOUNT_ID;
 
 const start = async () => {
+  console.log("Getting users account");
   const token = await getToken(process.env.API_KEY);
   const users = await getUsers(token, accountId);
+  const total = users.length;
+  let currentProgress = 0;
+
+  console.log(`Were found ${total} users`);
+  progress.init(total);
 
   let permissions = users.map(async (user) => {
     const access_groups = await getAccessGroups(token, accountId, user.user_id);
     user.access_groups = access_groups;
+    currentProgress++;
+    progress.update(currentProgress);
     return user;
   });
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const getToken = require("./helpers/iamToken");
 const getUsers = require("./helpers/getUsers");
 const getAccessGroups = require("./helpers/getAccessGroups");
+const ProgressBar = require("./utils/ProgressBar");
 
 module.exports = {
   getToken,
   getUsers,
   getAccessGroups,
+  ProgressBar,
 };
